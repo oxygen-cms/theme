952c14532efc9d7107aebb652914038500910a06
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-14 17:54:34 +1000

    Moved theming from `oxygen/core` to its own package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Oxygen - Preferences
+
+This repository contains the preferences framework.
+
+For more information visit the [Core](https://github.com/oxygen-cms/core) repository.
---
+++ src/BootThemeMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Oxygen\Theme;
+
+use Closure;
+
+class BootThemeMiddleware {
+
+    /**
+     * The theme manager.
+     *
+     * @var ThemeManager
+     */
+    protected $themeManager;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  ThemeManager  $themes
+     */
+    public function __construct(ThemeManager $themes)
+    {
+        $this->themeManager = $themes;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        $currentTheme = $this->themeManager->current();
+        if($currentTheme) {
+            $currentTheme->boot();
+        }
+
+        return $next($request);
+    }
+}
