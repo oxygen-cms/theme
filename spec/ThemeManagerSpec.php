44f2a54cbb118eeb6d2e2c5ae4e17eeb80104e0a
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-22 09:01:57 +1000

    Added tests

---
+++ spec/ThemeManagerSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace spec\Oxygen\Theme;
+
+use Oxygen\Theme\ThemeLoader;
+use Oxygen\Theme\Theme;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ThemeManagerSpec extends ObjectBehavior {
+
+    function let(ThemeLoader $loader) {
+        $this->beConstructedWith($loader);
+    }
+
+    function it_is_initializable() {
+        $this->shouldHaveType('Oxygen\Theme\ThemeManager');
+    }
+
+    function it_can_store_themes(Theme $theme) {
+        $theme->getKey()->willReturn('foo');
+        $this->register($theme);
+        $this->all()->shouldReturn(['foo' => $theme]);
+    }
+
+    function it_can_make_themes() {
+        $this->make([
+            'key' => 'fantasticTheme',
+            'name' => 'FANTASTIC Theme',
+            'provides' => [
+                'foo' => 'bar'
+            ]
+        ]);
+        $this->all()->shouldHaveCount(1);
+    }
+
+}
